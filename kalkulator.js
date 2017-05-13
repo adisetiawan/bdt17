27bd31c7c1810595a19b56914345ad7368eb9a49
Adi Setiawan <adisetiawan@gmail.com>
2017-05-13 15:28:27 +0800

    added calculator

---
+++ kalkulator.js
@@ -0,0 +1,22 @@
+var calculator = {
+	
+	tambah : function(a,b) {
+		return 
+	},
+	kurang : '',
+	kali  : '',
+	bagi  : '',
+	samadengan : function(a,b) {
+		//jika lebih besar a
+		if() {
+			return a + 'lebih besar dari ' + b;
+		} elseif() {
+
+		} else {
+
+		}
+	}
+};
+
+console.log(calculator.tambah(2,3));
+console.log(calculator.kurang(4,1));
